f968a527e2c6b131a4dfa9c99d784dc3c2baa785
Sai <sai@legolas1.utdallas.edu>
2022-12-27 03:13:23 -0500

    added makefile

--- ann_ip_cwb/compile.sh
+++
@@ -1,4 +0,0 @@
-g++ -pthread -O1 -I"/home/sai/Downloads/systemc-2.3.3/include/" -I"." -c ann_ip.cpp -o ann_ip.o
-g++ -pthread -O1 -I"/home/sai/Downloads/systemc-2.3.3/include/" -I"." -c main.cpp -o main.o
-g++ -pthread -O1 -I"/home/sai/Downloads/systemc-2.3.3/include/" -I"." -c tb_ann.cpp -o tb_ann.o
-g++ -pthread -o "ann_ip.exe"  ann_ip.o main.o tb_ann.o -lm "/home/sai/Downloads/systemc-2.3.3/lib-linux64/libsystemc.a"  -Wl,-rpath="/home/sai/Downloads/systemc-2.3.3/lib-linux64/libsystemc-2.3.3.so"
